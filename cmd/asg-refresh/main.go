b46012cd0266fe0e7447f00888e8132067ae9e1f
Nate Marks <npmarks@gmail.com>
2024-02-01 13:39:50 -0500

    initial

---
+++ cmd/asg-refresh/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/natemarks/asg-refresh/version"
+)
+
+func main() {
+	fmt.Println("asg-refresh version: ", version.Version)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/natemarks/asg-refresh
+
+go 1.21.5
